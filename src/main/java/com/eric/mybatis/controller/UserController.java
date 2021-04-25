b8362cd00ebcfb38fb54b4fa442a69f551403514
HUWEI <scorpio.huwei@gmail.com>
2021-04-25 23:50:28 +0800

    mybatis.README

--- src/main/java/com/eric/mybatis/controller/UserController.java
+++ src/main/java/com/eric/mybatis/controller/UserController.java
@@ -3,9 +3,9 @@ package com.eric.mybatis.controller;
 import com.eric.mybatis.entity.User;
 import com.eric.mybatis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
  * @AUTHOR SCORPIO.HU
  * @DATE 2021/4/25 下午10:25
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
