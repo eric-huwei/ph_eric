049b88651b92e55c3157f22ccd82f198bc23c631
HUWEI <scorpio.huwei@gmail.com>
2021-04-29 23:35:32 +0800

    sql execute duration

--- jdbc/src/main/java/com/eric/config/JdbcExceptionAop.java
+++ jdbc/src/main/java/com/eric/config/JdbcExceptionAop.java
@@ -2,9 +2,11 @@ package com.eric.config;
 
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
 
 /**
  * @DESCIRPTION JDBC AOP
@@ -15,11 +17,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class JdbcExceptionAop {
 
-    @Around("execution(* com.eric.mybatis.mapper..*.*(..))")
+    @Pointcut("execution(* com.eric.mybatis.mapper..*.*(..))")
+    public void pointCut() {
+    }
+
+    @Before("pointCut()")
+    public void before() {
+        Logger.getGlobal().info("point cut before: " + System.currentTimeMillis());
+    }
+
+    @After("pointCut()")
+    public void after() {
+        Logger.getGlobal().info("point cut after: " + System.currentTimeMillis());
+    }
+
+    @Around("pointCut()")
     public Object around(ProceedingJoinPoint joinPoint) {
         Object result = null;
         try {
+            long begin = System.nanoTime();
             result = joinPoint.proceed();
+            long end = System.nanoTime();
+
+            Logger.getGlobal().info("当前sqlId:"+joinPoint.getSignature().getName()+
+                    ",参数:"+ Arrays.toString(joinPoint.getArgs()) +" 执行耗时:"+(end - begin) / 1000000+"ms");
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
