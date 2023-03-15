87fcf091fb02c5f442fd511c992516668c753c8e
eric-huwei <scorpio.huwei@foxmail.com>
2023-03-15 10:15:03 +0800

    reentrantLock

--- jwtsso/src/main/java/com/sso/repository/SysUserRepository.java
+++ jwtsso/src/main/java/com/sso/repository/SysUserRepository.java
@@ -11,4 +11,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SysUserRepository extends JpaRepository<SysUser, Integer> {
 
     SysUser findByUsername(String username);
+
 }
