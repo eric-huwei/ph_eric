7a6fab6c6d4a8cee3370137e4fd3c61ef1b688f4
eric-huwei <scorpio.huwei@foxmail.com>
2023-03-25 11:37:00 +0800

    去除空格

--- jwtsso/src/main/java/com/sso/domain/MyUser.java
+++ jwtsso/src/main/java/com/sso/domain/MyUser.java
@@ -2,7 +2,6 @@ package com.sso.domain;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-
 import java.util.Collection;
 
 /**
