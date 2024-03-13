3971e5a252254c690dd775709d73a0a087d3972e
Eric <scorpio.huwei@foxmail.com>
2024-03-13 16:07:31 +0800

    hashcode中的位运算符

--- leetcode/src/main/java/daily/HashCode.java
+++ leetcode/src/main/java/daily/HashCode.java
@@ -11,4 +11,22 @@ public class HashCode {
         int h;
         return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
     }
+
+
+}
+class URShift {
+    public static void main(String[] args) {
+        int i = -1;
+        i >>>= 10;
+        System.out.println(i);
+        long l = -1;
+        l >>>= 10;
+        System.out.println(l);
+        short s = -1;
+        s >>>= 10;
+        System.out.println(s);
+        byte b = -1;
+        b >>>= 10;
+        System.out.println(b);
+    }
 }
