3ba7982de75954e836ee1907675b899b2b448c05
Eric <scorpio.huwei@foxmail.com>
2024-03-14 09:33:29 +0800

    日常算法：排序-有效的字母异位词

---
+++ leetcode/src/main/java/daily/IsAnagram.java
@@ -0,0 +1,23 @@
+package daily;
+
+import java.util.Arrays;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ */
+public class IsAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
