23cc3c6468f5373ba8dae1f31f6a65dc80f831ae
Eric <scorpio.huwei@foxmail.com>
2025-04-17 14:33:25 +0800

    项目拆分

--- eric-ollama/src/main/java/com/eric/EricOllamaApplication.java
+++ eric-ollama/src/main/java/com/eric/EricOllamaApplication.java
@@ -1,14 +1,13 @@
-package io.com.eric;
+package com.eric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloSpringBootApplication {
+public class EricOllamaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringBootApplication.class, args);
+		SpringApplication.run(EricOllamaApplication.class, args);
 	}
-
 
 }
---
+++ eric-ollama/src/test/java/com/eric/controller/EricOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
