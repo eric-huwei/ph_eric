b6a23e19dcd12a37fb2b67747fa999b30f1729b4
Eric <scorpio.huwei@foxmail.com>
2025-12-17 15:01:26 +0800

    项目拆分

--- eric-ssm/src/main/java/com/sso/SsmApplication.java
+++ eric-ssm/src/main/java/com/sso/SsmApplication.java
@@ -1,9 +1,11 @@
 package com.sso;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sso.mapper")
 public class SsmApplication {
 
     public static void main(String[] args) {
